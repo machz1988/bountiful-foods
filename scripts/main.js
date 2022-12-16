7b2595f7e7462718b0a29ca2f1199f05b4c5b0d7
machz1988 <machz2012@gmail.com>
2022-12-15 22:27:11 -0600

    first version home page

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+/** Hamburguer button changing class */
 function toggleMenu(){
     document.querySelector(".hamButton").classList.toggle("open");
     document.querySelector(".mainNav").classList.toggle("open");
@@ -5,3 +6,15 @@ function toggleMenu(){
 
 const x = document.querySelector(".hamButton");
 x.onclick = toggleMenu;
+
+/** Showing current year */
+const now = new Date();
+const year = now.getFullYear();
+
+const showyear = document.querySelector("#currentYear");
+showyear.textContent = year;
+
+/** Showing date last modified */
+const dateLastModified = document.lastModified;
+const showLastModified = document.querySelector("#lastModified");
+showLastModified.textContent = dateLastModified;
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,23 +1,68 @@
-const weather_date1 = document.querySelector(".weather-date1");
-const icon_elem1 = document.querySelector(".condition-icon1");
-const wind_speed1 = document.querySelector(".wind-speed1");
-const wind_chill1 = document.querySelector(".wind-chill1");
+const currentDate = Date.now();
+
+/** CURRENT WEATHER */
+const icon_elem = document.querySelector(".weather-condition");
+const wind_speed = document.querySelector(".humidity");
+const url1 = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad,CA,US&units=metric&appid=0874fda4b0c5e7c7d087937b085abaea";
+
+async function apiFetch1(){
+    try{
+        const response = await fetch(url1);
+        if (response.ok){
+            const data = await response.json();
+            displayResults1(data);
+        } else{
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults1(data){
+    //console.log(data);
+    const displayCurrentWeather = document.querySelector(".weather-condition");
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].description.split(" ");
+    for (let i=0; i<desc.length; i++){
+        desc[i] = desc[i].charAt(0).toUpperCase() + desc[i].slice(1);
+    }
+    const description = desc.join(" ");
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    img.setAttribute("src", iconsrc);
+    img.setAttribute("alt", `${desc} icon`);
+    figure.appendChild(img);
+    const caption = document.createElement("figcaption");
+    caption.style.textAlign = "center";
+    caption.innerHTML = `<strong>${description}</strong>`;
+    figure.appendChild(caption);
+    figure.style.textAlign = "center";
+    displayCurrentWeather.appendChild(figure);
+
+    const temp = data.main.temp.toFixed(0);
+    const temperature = document.createElement("span");
+    temperature.innerHTML = `<span><strong>${temp}</strong></span> &#8451;`;
+    temperature.style.fontSize = "2rem";
+    displayCurrentWeather.appendChild(temperature);
+
+    const humidity = data.main.humidity.toFixed(0);
+    const displayHumidity = document.querySelector(".humidity");
+    displayHumidity.textContent = `${humidity} %`;
+}
+
+apiFetch1();
+
+/** FORECAST */
 const url = "https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad,CA,US&units=metric&appid=0874fda4b0c5e7c7d087937b085abaea";
-//const url = "https://api.openweathermap.org/data/3.0/onecall?lat=33.158267&lon=-117.350996&exclude=daily&units=metric&appid=0874fda4b0c5e7c7d087937b085abaea";
-
-//let temp_c = 0;
-//let wind_speed_kmh = 0;
-
-const now = Date.now();
-//console.log(now);
-console.log(parseInt(now/1000)+86400-72000);
 
 async function apiFetch(){
     try{
         const response = await fetch(url);
         if (response.ok){
             const data = await response.json()
-            console.log(data);
+            //console.log(data);
+            showResults(data);
         } else{
             throw Error(await response.text());
         }
@@ -26,4 +71,85 @@ async function apiFetch(){
     }
 }
 
+function showResults(data){
+    const forecast = document.querySelector(".forecast");
+    
+    for (let i = 5; i < 22; i+=8){
+        const article = document.createElement("article");
+
+        // day of week
+        const h3 = document.createElement("h3");
+        const date = new Date(data.list[i].dt_txt)        
+        switch (date.getDay()){
+            case 0:
+                h3.textContent = "Sunday";
+                break;
+            case 1:
+                h3.textContent = "Monday";
+                break;
+            case 2:
+                h3.textContent = "Tuesday";
+                break;
+            case 3:
+                h3.textContent = "Wednesday";
+                break;
+            case 4:
+                h3.textContent = "Thursday";
+                break;
+            case 5:
+                h3.textContent = "Friday";
+                break;
+            case 6:
+                h3.textContent = "Saturday";
+                break;
+        }
+        article.appendChild(h3);
+
+        // hour
+        const spanHour = document.createElement("span");
+        spanHour.textContent = data.list[i].dt_txt.substring(11);
+        article.appendChild(spanHour);
+        
+        // weather icon
+        const figure = document.createElement("figure");        
+        const iconsrc = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`;
+        const img = document.createElement("img");        
+        img.setAttribute("src", iconsrc);
+        img.setAttribute("alt", data.list[i].weather[0].description);
+        //img.style.margin = "0 auto";
+        const displayDescription = document.createElement("figcaption");
+        const desc = data.list[i].weather[0].description.split(" ");
+        for (let i=0; i<desc.length; i++){
+            desc[i] = desc[i].charAt(0).toUpperCase() + desc[i].slice(1);
+        }
+        const description = desc.join(" ");
+        displayDescription.innerHTML = `<strong>${description}</strong>`;
+        figure.appendChild(img);
+        figure.appendChild(displayDescription);
+        article.appendChild(figure);
+
+        //temp max
+        const spanTmax = document.createElement("span");
+        spanTmax.innerHTML = `${data.list[i].main.temp_max} &#8451;`;
+        article.appendChild(spanTmax);
+
+        // temp min
+        const spanTmin = document.createElement("span");
+        spanTmin.innerHTML = `${data.list[i].main.temp_min} &#8451;`;
+        article.appendChild(spanTmin);
+        
+        article.style.display = "flex";
+        article.style.flexDirection = "column";
+        article.style.borderTop = "0.05rem dashed var(--primary-color)";
+        forecast.appendChild(article);
+    }
+    //console.log(data.list);
+}
+
 apiFetch();
+
+/** Showing number of drinks served */
+const showDrinksServed = document.querySelector("#numberDrinksServed");
+const drinksServed = Number(window.localStorage.getItem("drinks-served-number"));
+showDrinksServed.textContent = drinksServed;
+console.log(drinksServed);
